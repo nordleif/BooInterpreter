eb61cf30f2ea4aa842f6bf7ef56db3006aff0cba
Bo Nordleif <bno@frontmatec.com>
2017-12-12 21:13:22 +0100

    app

--- BooInterpreter.App/Program.cs
+++ BooInterpreter.App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,52 @@ namespace BooInterpreter
 {
     class Program
     {
+        #region Static Members
+
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, e) => {
+                try
+                {
+                    var assembly = Assembly.GetExecutingAssembly();
+                    var assemblyName = new AssemblyName(e.Name);
+                    var fileName = assemblyName.Name + ".dll";
+                    var resources = assembly.GetManifestResourceNames().Where(s => s.EndsWith(fileName));
+                    if (resources.Any())
+                    {
+                        var resourceName = resources.First();
+                        using (var stream = assembly.GetManifestResourceStream(resourceName))
+                        {
+                            if (stream == null)
+                                return null;
+
+                            var buffer = new byte[stream.Length];
+                            try
+                            {
+                                stream.Read(buffer, 0, buffer.Length);
+                                return Assembly.Load(buffer);
+                            }
+                            catch (IOException)
+                            {
+                                return null;
+                            }
+                            catch (BadImageFormatException)
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+                return null;
+            };
+        }
+
+        #endregion
+
         static void Main(string[] args)
         {
             var prompt = ">>> ";
